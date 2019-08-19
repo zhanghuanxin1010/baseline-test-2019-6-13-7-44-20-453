41827bb5917a42e93a05ce7b22c4472b571e7ba4
unknown <1012151538@qq.comgit config --global user.name >
2019-08-19 18:44:04 +0800

    new

--- practice/section-1/practice-1.js
+++ practice/section-1/practice-1.js
@@ -1,5 +1,20 @@
 'use strict';
 
 function collectSameElements(collectionA, collectionB) {
-  return '实现练习要求，并改写该行代码。';
+	var B=new Array();
+	
+	for(var i=0;i<collectionA.length;i++)
+	{
+		for(var j=0;j<collectionB.length;j++)
+		{
+			if(collectionA[i]==collectionB[j])
+			{
+				B.push(collectionA[i]);
+			}
+				
+		}
+	}
+	
+	
+  return B;
 }
